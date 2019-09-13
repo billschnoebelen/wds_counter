7b583acd48986578ba90e19deec0b33b57edb059
Bill Schnoebelen <Bill@circuitcase.com>
2019-09-12 22:42:17 -0700

    component props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from "react";
+import Counter from "./Counter";
 
 function App() {
-  return <h1>null</h1>
+  console.log("Render App");
+  return <Counter initialCount={4} />;
 }
 
 export default App;
